f59f601dcef1ffc1dfc837c0cce8bbe4b7e179da
Alberto Paro <alberto.paro@gmail.com>
2023-04-09 20:11:10 +0200

    Initial refactory of tests

--- elasticsearch-tests/src/test/scala/zio/elasticsearch/orm/ORMClassTest.scala
+++ elasticsearch-tests/src/test/scala/zio/elasticsearch/orm/ORMClassTest.scala
@@ -1,19 +1,30 @@
 /*
- * Copyright 2023 - Accenture Data & AI. All Rights Reserved.
+ * Copyright 2019-2023 Alberto Paro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package zio.elasticsearch.orm
 
 import java.time.{ LocalDate, LocalDateTime, OffsetDateTime }
 
-import fixures.models.Person
 import zio.elasticsearch.geo.GeoPoint
 import zio.json._
 import zio.schema.elasticsearch.annotations.{ IndexName, Ip, Keyword, Nested }
 import zio.schema.{ DeriveSchema, Schema }
 
 final case class ORMClassTest(id: String, name: String, age: Int, @Keyword nick: Option[String] = None)
-object ORMClassTest extends ElasticSearchMeta[ORMClassTest] {
+object ORMClassTest  {
   implicit final val decoder: JsonDecoder[ORMClassTest] =
     DeriveJsonDecoder.gen[ORMClassTest]
   implicit final val encoder: JsonEncoder[ORMClassTest] =
@@ -42,7 +53,7 @@ final case class ORMAllMappingTest(
   point: GeoPoint = GeoPoint(0, 0)
 )
 
-object ORMAllMappingTest extends ElasticSearchMeta[ORMAllMappingTest] {
+object ORMAllMappingTest {
   implicit final val decoder: JsonDecoder[ORMAllMappingTest] =
     DeriveJsonDecoder.gen[ORMAllMappingTest]
   implicit final val encoder: JsonEncoder[ORMAllMappingTest] =
